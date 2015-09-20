49e05f29f7d725359fe7ccadab71424f2bd65429
Jiahui <goodzhoujiahui@gmail.com>
2015-09-20 17:48:30 +0800

    add simpleness function

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,15 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
     }
+    
+    var isInTheMiddleOfTypingNumber = false;
+    
+    @IBAction func apendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if isInTheMiddleOfTypingNumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            isInTheMiddleOfTypingNumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        switch operation {
+        case "×": performOperatin {$0 * $1}
+        case "÷": performOperatin {$1 / $0}
+        case "−": performOperatin {$1 - $0}
+        case "+": performOperatin {$0 + $1}
+        default :break
+        }
+    }
+    
+    func performOperatin(operation: (Double, Double) -> Double) {
+        if operandStank.count >= 2 {
+            displayValue = operation(operandStank.removeLast(), operandStank.removeLast())
+        }
+    }
+    
+//    func multiply (op1:Double, op2:Double) -> Double {
+//        return op1 * op2;
+//    }
+    
+    var operandStank = Array<Double>()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func enter(sender: UIButton) {
+        isInTheMiddleOfTypingNumber = false
+        operandStank.append(displayValue)
+        print("operandStank = \(operandStank)")
     }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            isInTheMiddleOfTypingNumber = false
+        }
+    }
+    
+    
 
 
 }
